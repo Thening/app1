e07d4ea734ed92adb025293415ac3a3d04427494
thening Diouf <thening@Rolands-MacBook-Pro.local>
2020-07-06 06:49:55 +0000

    add routing

--- hello.js
+++ hello.js
@@ -1,4 +1,6 @@
 const http = require("http");
+/*
+// simple without routing
 
 http.createServer((request, response) => {
     response.writeHead(200, {
@@ -8,4 +10,26 @@ http.createServer((request, response) => {
     response.write('Hello, World!\n');
 
     response.end();
+}).listen(1337);
+*/
+// routing
+
+var routes = {
+    '/': function index (request, response) {
+        response.writeHead(200);
+        response.end('Hello, World!');
+    },
+    '/foo': function foo (request, response) {
+        response.writeHead(200);
+        response.end('you are now viewing "foo"');
+    }
+}
+
+http.createServer(function(request, response) {
+    if (request.url in routes) {
+        return routes[request.url] (request, response)
+    }
+
+    response.writeHead(404);
+    response.end(http.STATUS_CODES[404]);
 }).listen(1337);
